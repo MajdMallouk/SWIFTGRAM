c1f39ffff69ae920b481d6c5619cc293d208f9b7
Majd Mallouk <majdmallouk365@gmail.com>
2024-08-18 12:57:55 +0300

    some minor changes

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        About
+    </x-slot:heading>
     <h1>about us...we're alive</h1>
 </x-layout>
--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,3 +1,6 @@
 <x-layout>
+    <x-slot:heading>
+        Contact
+    </x-slot:heading>
     <h1>Contact us...don't worry...never responding</h1>
 </x-layout>
