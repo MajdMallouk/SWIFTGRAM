49ac06f3d800a90c2033dd2681c872639269524c
MajdMallouk <majdmallouk365@gmail.com>
2024-08-23 00:35:46 +0300

    Added more complex Eloquent relationships

---
+++ app/Models/Job.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Job extends Model {
+    use hasFactory;
+
+        protected $table = 'job_listings';
+        protected $fillable = ['title', 'salary'];
+
+        public function employer()
+        {
+            return $this->belongsTo(Employer::class);
+            }
+    }
